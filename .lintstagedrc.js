82c5b507f019a994f8b22f51433fd7f2dbfbd75b
Danyelle Amarante <90638175+Danyelleac@users.noreply.github.com>
2024-01-29 08:06:26 -0300

    config-lintstadedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['prettier --write'],
-  '*.{js,jsx,ts,tsx,css,scss}': ['git add'],
-  '*.{json,md,html,css,scss,yml,yaml}': ['prettier --write', 'git add']
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`
+  ]
 }
