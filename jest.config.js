d88c909c5e9ff2922bf73d97658674d0ff7cad78
Danyelle Amarante <90638175+Danyelleac@users.noreply.github.com>
2024-01-30 08:54:42 -0300

    configure styled-component

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom', //ambiente de teste
   testPathIgnorePatterns: ['/node_modules/', '/.next/'], //ignorar essas pastas
   collectCoverage: true, //coletar cobertura de testes
-  collectCoverageFrom: ['src/**/*.ts(x)?'], //coletar cobertura de testes-lugar onde estão
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?', //a pasta onde ele vai procurar os testes
+    '!src/app/**', // ignorar configurações do next, apenas testalas no teste E2E
+    '!src/lib/registry.tsx' //ignorar as configurações da lib , pois não faz sentido testar
+  ], //coletar cobertura de testes-lugar onde estão
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], //configuração do jest antes de tudo
   modulePaths: ['<rootDir>/src/'], //configuração de caminhos- colocar arquivos dentro de src
   transform: {
---
+++ src/components/main/styles.ts
@@ -0,0 +1,16 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+  background-color: #06092b;
+  color: #fff;
+  padding: 3rem;
+`
